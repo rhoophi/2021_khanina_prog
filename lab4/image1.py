9a96e53be9f960e3b12585c7c87ed5e7b90aaf29
rhoophi <khanina.va@phystech.edu>
2021-09-28 18:41:58 +0300

    update lab4 9/28/2021 6.41pm

---
+++ lab4/image1.py
@@ -0,0 +1,30 @@
+import pygame
+import pygame.draw as draw
+
+pygame.init()
+
+FPS = 30
+screen = pygame.display.set_mode((400, 500))
+draw.rect(screen, (255, 255, 255), (0, 0, 400, 500))
+
+draw.circle(screen, (247, 237, 186), (200, 250), 100)
+draw.circle(screen, (0, 0, 0), (200, 250), 100, 1)
+draw.rect(screen, (0, 0, 0), (150, 300, 100, 10))
+draw.circle(screen, (250, 127, 127), (150, 250), 30)
+draw.circle(screen, (250, 127, 127), (250, 250), 20)
+draw.circle(screen, (0, 0, 0), (150, 250), 20)
+draw.circle(screen, (0, 0, 0), (250, 250), 10)
+draw.polygon(screen, (0, 0, 0), ([(100, 190), (175, 210), (190, 240)]))
+draw.polygon(screen, (0, 0, 0), ([(210, 210), (230, 190), (300, 190)]))
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
